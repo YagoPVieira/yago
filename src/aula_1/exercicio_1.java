0d21d8fde2246818ccc9c0fb52864be3053a675e
YagoPVieira <yagopvieira@gmail.com>
2024-09-11 21:34:58 -0300

    primeiro commit

---
+++ src/aula_1/exercicio_1.java
@@ -0,0 +1,11 @@
+package aula_1;
+
+public class exercicio_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("OLa mundo");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+module primeiro_projeto {
+}
